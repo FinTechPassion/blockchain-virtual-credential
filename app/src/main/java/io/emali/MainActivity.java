4cef2195c5b1ad95f44ac6f80fc9cd5aa6385d22
Kavis Cho <kencho3w0w@gmail.com>
2018-06-22 15:45:50 +0800

    Kavis commit

--- app/src/main/java/io/emali/MainActivity.java
+++ app/src/main/java/io/emali/MainActivity.java
@@ -2,15 +2,30 @@ package io.emali;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int SIGN_IN_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Should be Jacky and wing
-        System.out.print("Jacky");
-        System.out.print("wing");
+
+        FirebaseApp.initializeApp(this);
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+
+
+        } else {
+            Log.d("Main","Logged in");
+
+        }
+
     }
 }
