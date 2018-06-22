33b329d696c4423698ca938a893198e600c649ef
jacky <17038365d@connect.polyu.hk>
2018-06-22 15:24:23 +0800

    First commit

--- app/src/main/java/io/emali/MainActivity.java
+++ app/src/main/java/io/emali/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.print("Kavis");
+
+        System.out.print("Jacky");
+
     }
 }
